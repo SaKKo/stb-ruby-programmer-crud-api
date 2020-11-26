3280c026baa3a871f30006ad3c73631528717de2
SaKKo <saklism@gmail.com>
2020-11-26 13:24:56 +0700

    updated scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
   get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show'
   post 'users', to: 'users#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_080422) do
+ActiveRecord::Schema.define(version: 2020_11_26_055650) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.integer "pages"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "title"
